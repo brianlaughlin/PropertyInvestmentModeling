587e34b718c2830962a5af58955bab1bd52ee929
brian@inetoc.com <DXt8E0s9wRObTCp6Vf8U>
2021-01-09 17:22:05 -0800

    added form for rental properites

---
+++ src/main/java/com/brianlaughlin/invesmentmodeling/controller/PropertyForm.java
@@ -0,0 +1,18 @@
+package com.brianlaughlin.invesmentmodeling.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PropertyForm {
+
+//    @Autowired
+//    private PropertyForm propertyForm;
+
+    @RequestMapping(value ="/form")
+    public String mainForm(){
+
+        return "form";
+    }
+}
