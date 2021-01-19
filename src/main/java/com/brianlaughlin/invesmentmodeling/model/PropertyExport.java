5acde399a208c9ae280317a00193e2405317dac5
brian@inetoc.com <DXt8E0s9wRObTCp6Vf8U>
2021-01-18 20:50:16 -0800

    Remove allArgContructor

--- src/main/java/com/brianlaughlin/invesmentmodeling/model/PropertyExport.java
+++ src/main/java/com/brianlaughlin/invesmentmodeling/model/PropertyExport.java
@@ -1,12 +1,10 @@
 package com.brianlaughlin.invesmentmodeling.model;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
-@AllArgsConstructor  // Not sure if I need this
 public class PropertyExport {
 
     String street;
