f9313ec34b9ef67a4d36f1ccfdb2986289d34a9f
brian@inetoc.com <DXt8E0s9wRObTCp6Vf8U>
2021-02-12 23:18:27 -0800

    Added some javadoc to class.

--- src/main/java/com/brianlaughlin/invesmentmodeling/calculators/CalcCapRate.java
+++ src/main/java/com/brianlaughlin/invesmentmodeling/calculators/CalcCapRate.java
@@ -13,11 +13,23 @@ public class CalcCapRate implements Calculator {
                 getInvestment(property);
     }
 
+    /**
+     * <p>Internal method to help simplify code in calculating property investment</p>
+     * @param property
+     * @return
+     */
     private double getInvestment(Property property) {
         return property.getTakeOutCost() + property.getPurchasePrice() +
                 property.getRehab();
     }
 
+    /**
+     * <p>Cap Rate calculation that taks into account on-going maintenance costs.</p>
+     * <i>It's typical to estiamte around 10 to 15 percent of monthly rents as deferred maintenance costs.</i>
+     * <b>This will be a more conservative estimate.</b>
+     * @param property
+     * @return
+     */
 
     public double calcWithMaintenance(Property property) {
 
