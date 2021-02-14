8dbe80d9b5efae1de4854be8c415eb3bb6e1605d
brian@inetoc.com <DXt8E0s9wRObTCp6Vf8U>
2021-02-13 21:15:35 -0800

    Added enum for rehab states.

---
+++ src/main/java/com/brianlaughlin/invesmentmodeling/calculators/rehab/RehabState.java
@@ -0,0 +1,11 @@
+package com.brianlaughlin.invesmentmodeling.calculators.rehab;
+/*
+LIGHT = Carpet & Paint
+AVERAGE = Carpet, Paint, Kitchen & Bath
+HEAVY = Gut Everything / New
+ */
+public enum RehabState {
+    LIGHT,
+    AVERAGE,
+    HEAVY
+}
